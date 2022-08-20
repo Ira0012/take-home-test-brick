bbbaaf35fdc697206f910fd14ed101d35ae1f714
Siti Munawaroh <siti.munawaroh02@yahoo.co.id>
2022-08-20 13:29:43 +0700

    fix count

--- src/main/java/io/onebrick/take_home_test_brick/Main.java
+++ src/main/java/io/onebrick/take_home_test_brick/Main.java
@@ -15,7 +15,7 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        int count = 0;
+        int count = 1;
         int pgCnt = 1;
         List<String[]> csvData = new ArrayList<>();
         String[] header = new String[7];
